796b2676909a0cf250e3d00d72012042606e5a2a
Swaroop Kammath <103194820+cluelescoder@users.noreply.github.com>
2023-06-21 19:01:37 +0530

    Update requestMethods.js

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL= "http://localhost:5000/api/"
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ODk1ZDYwNzE4OGY5N2VjZjI0NTc5OCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4NzA4OTA0NiwiZXhwIjoxNjg3MzQ4MjQ2fQ.6zYZuDKGUQ2_eahnlZIPtzyBvDet56q8zP-YCH-4nKA"
+// const TOKEN = ""
 
 export const publicRequest= axios.create({
     baseURL:BASE_URL,
